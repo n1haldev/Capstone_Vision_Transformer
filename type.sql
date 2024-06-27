92ded3a791e4ac55de369272a3704e01dc85cba7
Naren-NS <106026507+Naren-NS@users.noreply.github.com>
2024-06-27 16:39:18 +0530

    create this database in MySQL before running svelte/go app

--- type.sql
+++ type.sql
@@ -1,1 +1,8 @@
 create database go_auth;
+
+use go_auth;
+
+select * from go_auth.regs; 
+-- above line should show the table with email passowrd name etcetra
+
+-- D:\Capstone_Vision_Transformer\og-auth\pkg\config\app.go, over here, change it to your respective MySQL email password and username on line 13
