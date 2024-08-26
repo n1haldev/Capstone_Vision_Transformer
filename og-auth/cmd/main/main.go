7aa81d1eb204deeb3cdddc863737a2cc8ac55ed8
Naren-NS <106026507+Naren-NS@users.noreply.github.com>
2024-08-26 18:32:03 +0530

    Has the vision transformer code including transformer encoder, tested on dataset->https://github.com/dusty-nv/jetson-inference/blob/master/docs/pytorch-plants.md

--- og-auth/cmd/main/main.go
+++ og-auth/cmd/main/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"github.com/gorilla/mux"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/naren/og-auth/pkg/routes"
+    "log"
+    "net/http"
+    "github.com/gorilla/handlers"
+    "github.com/gorilla/mux"
+    "github.com/naren/og-auth/pkg/routes"
 )
 
-func main(){
-	r:=mux.NewRouter()
-	routes.RegisterRoutes(r)
+func main() {
+    router := mux.NewRouter()
+    routes.RegisterRoutes(router)
 
-	r.PathPrefix("/").Handler(http.StripPrefix("/",http.FileServer(http.Dir("D:/og-auth/"))))
-	log.Fatal(http.ListenAndServe("localhost:9010",r))
+    // Enable CORS
+    headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+    methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
+    origins := handlers.AllowedOrigins([]string{"*"}) // Change "*" to your frontend's domain for production
+
+    log.Fatal(http.ListenAndServe(":8000", handlers.CORS(headers, methods, origins)(router)))
 }
--- og-auth/go.mod
+++ og-auth/go.mod
@@ -5,7 +5,9 @@ go 1.22.4
 require golang.org/x/crypto v0.24.0
 
 require (
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
