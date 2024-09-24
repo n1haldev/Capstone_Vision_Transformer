04c0082ec86ebf794827f238edfb2b84c668c6a6
Nihal T M <nihaltmdev@gmail.com>
2024-09-24 17:39:03 +0530

    Added Navbar component to lib

---
+++ og-auth-frontend/src/lib/Navbar.svelte
@@ -0,0 +1,55 @@
+<script>
+    export let activePage = '';
+  </script>
+  
+  <style>
+    nav {
+      background-color: #333;
+      color: white;
+      padding: 1rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .logo img {
+      width: 5%;
+      height: 5%;
+      border-radius: 100%;
+    }
+  
+    .nav-links {
+      display: flex;
+      gap: 1rem;
+    }
+  
+    .nav-link {
+      color: white;
+      border-radius: 100%;
+      text-decoration: none;
+      padding: 0.5rem 1rem;
+      border-radius: 4px;
+    }
+  
+    .nav-link:hover {
+      background-color: #555;
+    }
+  
+    .nav-link.active {
+      background-color: #007BFF;
+    }
+  </style>
+  
+  <nav>
+    <div class="logo">
+      <a href="/">
+        <img src="./logo.png" alt="">
+      </a>
+    </div>
+    <div class="nav-links">
+      <a href="/register" class="nav-link {activePage === 'register' ? 'active' : ''}">Register</a>
+      <a href="/about" class="nav-link {activePage === 'about' ? 'active' : ''}">About</a>
+      <a href="/contact" class="nav-link {activePage === 'contact' ? 'active' : ''}">Contact</a>
+      <a href="/login" class="nav-link {activePage === 'login' ? 'active' : ''}">Login</a>
+    </div>
+  </nav>
