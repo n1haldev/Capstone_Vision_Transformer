142f374888236c03ae1148896ecf36be17ae13ba
Nihal T M <nihaltmdev@gmail.com>
2024-09-25 09:29:09 +0530

    Created a minimal contact page and a home page

--- og-auth-frontend/src/routes/+page.svelte
+++ og-auth-frontend/src/routes/+page.svelte
@@ -1,2 +1,70 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Navbar from "$lib/Navbar.svelte";
+  import { goto } from '$app/navigation';
+  
+</script>
+
+<style>
+  body {
+    background: url('./plant_bgi.jpg') no-repeat center center fixed;
+    background-size: cover;
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  .hero {
+    text-align: center;
+    padding: 2rem 2rem;
+    background: rgba(0, 0, 0, 0.5);
+    height: 80vh;
+    color: white;
+  }
+
+  .hero h1 {
+    font-size: 3rem;
+    margin-bottom: 0.5rem;
+  }
+
+  .hero p {
+    font-size: 1.5rem;
+  }
+
+  .hero button {
+    padding: 0.5rem;
+    margin: 1rem;
+    background-color: aqua;
+    color: black;
+    border-radius: 10%;
+    border-color: #555;
+  }
+
+  .hero button:hover {
+    background-color: lightskyblue;
+    transform: scale(1.1);
+    transition: transform 0.5s ease-in-out, box-shadow 0.5s ease-in-out;
+  }
+</style>
+
+<body>
+  <Navbar activePage="home" />
+  <div class="hero">
+    <h1>Welcome to PlantVision</h1>
+    <p>Empowering Farmers and Academics with Advanced Plant Detection</p>
+    <br>
+    <button on:click={() => goto('/register')}>Register</button>
+    <button on:click={() => goto('/login')}>Login</button>
+  </div>
+  <!-- <div class="services">
+    <div class="service">
+      <img src="./weed_detection_icon_resized.png" alt="Weed Detection">
+      <h2>Weed Detection for Farmers</h2>
+      <p>Utilize our advanced YOLOv5 model to detect weeds in your field and improve crop yield.</p>
+    </div>
+    <div class="service">
+      <img src="./plant_species_identification.jpg" alt="Plant Species Identification">
+      <h2>Plant Species Identification for Academics</h2>
+      <p>Identify plant species accurately for your research and academic projects using our cutting-edge technology.</p>
+    </div>
+  </div> -->
+</body>
---
+++ og-auth-frontend/src/routes/contact/+page.svelte
@@ -0,0 +1,80 @@
+<script>
+    import Navbar from "$lib/Navbar.svelte";
+</script>
+
+<style>
+    
+    body {
+        background: url('./plant_bgi.jpg') no-repeat center center fixed;
+        background-size: cover;
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+
+    .hero {
+        text-align: center;
+        padding: 2rem 2rem;
+        background: rgba(0, 0, 0, 0.5);
+        height: 80vh;
+        color: white;
+    }
+
+    .hero h1 {
+    font-size: 3rem;
+    margin-bottom: 0.5rem;
+    }
+
+    .hero p {
+        font-size: 1.5rem;
+    }
+
+    .contacts {
+        width: 10;
+        height: 10;
+        margin: 1rem;
+    }
+
+    .contact-list {
+        background-color: white;
+        margin: 1rem auto;
+        display: flex;
+        justify-content: center;
+        border-radius: 10%;
+        padding: 1rem;
+        width: 20%;
+    }
+
+    @media (max-width: 800px) {
+        .contact-list {
+            width: 50%;
+        }
+    }
+
+    @media (max-width: 600px) {
+        .contact-list {
+            width: 80%;
+        }
+    }
+
+</style>
+
+<body>
+    <Navbar activePage="contact" />
+    <div class="hero">
+        <h1>Contact Us</h1>
+        <p>Here are some ways you can reach out to us</p>
+        <br>
+        <div class="contact-list">
+            <a href="https://www.linkedin.com/in/nihal-tangaturu-murthy-601919164/" target="_blank" rel="noopener noreferrer" >
+                <img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" class="contacts">
+            </a>
+            <a href="mailto:nihaltm2002@gmail.com" target="_blank" rel="noopener noreferrer">
+                <img src="https://img.icons8.com/fluent/48/000000/gmail.png" alt="Gmail" class="contacts" />
+            </a>
+            <a href="https://github.com/n1haldev" target="_blank" rel="noopener noreferrer">
+                <img src="https://img.icons8.com/fluent/48/000000/github.png" alt="GitHub" class="contacts" />
+            </a>
+        </div>
+    </div>
+</body>
